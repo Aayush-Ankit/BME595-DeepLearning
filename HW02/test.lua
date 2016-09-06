15e1eddd788e253e98606ff1aa42a24ecf585a56
Aayush-Ankit <Aayush-Ankit@users.noreply.github.com>
2016-09-06 10:13:41 -0400

    Add files via upload

---
+++ HW02/test.lua
@@ -0,0 +1,23 @@
+-- testing the NN
+torch.setdefaulttensortype('torch.FloatTensor')
+tbl = {}
+
+a = require'NeuralNetwork'
+
+theta_num = torch.Tensor({3,2,2})
+
+a.build(theta_num)
+
+in_vec = torch.Tensor(theta_num[1],2)
+out_vec1 = in_vec:t()
+for i = 1,(theta_num:size(1)-1) do
+   out_vec1 = torch.cat(out_vec1,torch.ones(out_vec1:size(1),1),2) * a.getLayer(i)
+end
+(((out_vec1:mul(-1)):exp()):add(1)):pow(-1)
+out_vec2 = a.forward(in_vec)
+
+print (out_vec1)
+print (out_vec2)
+value = (torch.dist(out_vec1, out_vec2) == 0) and 'True' or 'False'
+print(value)
+--assert(torch.dist(out_vec1, out_vec2) < 1e-3, 'NN Error')
