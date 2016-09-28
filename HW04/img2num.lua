1d725dbf500bab280c1bb1777fbc574c10cf9657
Aayush-Ankit <Aayush-Ankit@users.noreply.github.com>
2016-09-27 23:33:59 -0400

    Add files via upload

--- HW04/img2num.lua
+++ HW04/img2num.lua
@@ -9,21 +9,21 @@ local mnist = require 'mnist'
 local trainset = mnist.traindataset()
 local testset = mnist.testdataset()
 local trainset_size = trainset.size
-local testset_size = testset.size
+--local testset_size = testset.size
+local testset_size = 500
 
 -- training spec. variables
-local batch_size = 1
-local max_iter = 3000 -- one mini-batch per iteration
-local eta = 0.3
-local epsilon = 0.1
+local batch_size = 60
+local max_iter = 1000000 -- one mini-batch per iteration
+local eta = 0.15
+local epsilon = 0.05
 
 -- network parameters
 local in_size = 784
-local num_hidden = 10
+local num_hidden = 50
 local num_class = 10
 local net_size = {in_size, num_hidden, num_hidden, num_class}
 local num_layers = #(net_size)-1
-local theta_best = {} -- stores the model with least Jcv
 
 -- function to convert a labels to onehot encodings
 local function onehot (x)
@@ -32,16 +32,25 @@ local function onehot (x)
    return label
 end
 
+-- function to predict the output digit (ineteger) from the neuron outputs
 local function pred_digit (x)
    local prob, pred
    prob, pred = torch.max(x,1)
    return (pred-1)
 end
 
+-- function to evaluate the classfication accuracy
 local function pred_acc (x, labels)
    local pred = pred_digit(x)
    local labels = pred_digit(labels)
    return (torch.eq(pred:byte(), labels:byte()):sum())/x:size(2)*100
+end
+
+-- build the neural network
+local net = nn.Sequential()
+for i = 1,(#(net_size)-1) do
+   net:add(nn.Linear(net_size[i], net_size[i+1]))
+   net:add(nn.Sigmoid())
 end
 
 -- function to train the neural network
@@ -56,13 +65,8 @@ function img2num.train ()
    local cv_acc = torch.zeros(max_iter)
    local perm
 
-   -- build the network before training starts
-   loss = nn.MSECriterion()
-   net = nn.Sequential()
-   for i = 1,(#(net_size)-1) do
-      net:add(nn.Linear(net_size[i], net_size[i+1]))
-      net:add(nn.Sigmoid())
-   end
+   -- define the loss fucntion (error function)
+   local loss = nn.MSECriterion()
 
    -- begin training
    for j = 1,max_iter do
@@ -84,75 +88,50 @@ function img2num.train ()
          label_t[{{} ,i}] = onehot(trainset[idx].y)
       end
 
-      -- training
-      invec_t = invec_t:view(in_size)
-      local tr_out = net:forward(invec_t)
-      tr_error[j] = loss:forward(tr_out, label_t)
-      local gradLoss = loss:backward(tr_out, label_t)
-      net:zeroGradParameters()
-      net:backward(invec_t, gradLoss)
+      -- training with batches
+      net:zeroGradParameters() -- make the parameters zero before each batch
+      for k = 1,batch_size do
+         local invec_tt = invec_t[{{} ,k}]
+         local label_tt = label_t[{{} ,k}]
+         local tr_out = net:forward(invec_tt)
+         tr_error[j] = loss:forward(tr_out, label_tt)
+         local gradLoss = loss:backward(tr_out, label_tt)
+         net:backward(invec_tt, gradLoss)
+      end
       net:updateParameters(eta)
 
-      -- plot the training error
-      gnuplot.plot ('Training Error', tr_error[{{1,j}}])
-
-      -- run the model on crossvalidation set to find accuracy
-      --[[local invec_cv_t = torch.zeros(in_size, testset_size)
+      -- run the model on crossvalidation set to find jcv and accuracy
       local label_cv_t = torch.zeros(num_class, testset_size)
+      local cv_out = torch.zeros(num_class, testset_size)
       for i = 1,testset_size do
-         local data = (testset[i].x):float()
-         data = data/255
-         invec_cv_t[{{} ,i}] = data:view(in_size,1)
+         local data = (((testset[i].x):float())/255):view(in_size)
          label_cv_t[{{}, i}] = onehot(testset[i].y)
+         cv_out[{{}, i}] = net:forward(data)
+         cv_error[j] = loss:forward(cv_out[{{}, i}], label_cv_t[{{}, i}])
       end
 
-      local cv_out = nn.forward(invec_cv_t)
-      cv_error[j] = (((cv_out - label_cv_t):pow(2)):sum())/(cv_out:size(2))
-
-      if (j == 1) then
-         min_cv_idx = j
-      elseif (cv_error[j] < cv_error[min_cv_idx]) then
-         min_cv_idx = j
-         -- save the best theta model
-         local theta_tbl = {}
-         for i = 1,num_layers do
-            theta_tbl[i] = nn.getLayer(i)
-         end
-         theta_best = theta_tbl
-      end--]]
+      -- stopping condition for training
+      --if (cv_error[j] < epsilon) then break end
 
       -- store th cv accuracy
-      --cv_acc[j] = pred_acc (cv_out, label_cv_t)
-      --gnuplot.figure(1)
-      --gnuplot.plot('Classification Accurcay', cv_acc[{{1,j}}])
+      cv_acc[j] = pred_acc (cv_out, label_cv_t)
+      gnuplot.figure(1)
+      gnuplot.plot('Classification Accurcay', cv_acc[{{1,j}}])
 
       --print ("Iteration:", k)
       --print ("tr_error:", tr_error[k-1])
-      --gnuplot.figure(2)
-      --gnuplot.plot({'Training Error',tr_error[{{1,j}}]}, {'Crossvalidation Error',cv_error[{{1,j}}]})
+      gnuplot.figure(2)
+      gnuplot.plot({'Training Error',tr_error[{{1,j}}]}, {'Crossvalidation Error',cv_error[{{1,j}}]})
    end
 
 end
 
 function img2num.forward (x)
-         local data = x:float()
+         local data = (x:float()):view(in_size)
          data = data/255
 
-         -- assign the theta_best to the neural network
-         nn.build(net_size)
-         for i = 1,num_layers do
-            -- get the layer
-            theta = nn.getLayer(i)
-            -- assign the theta_best to this theta by referencing
-            for m = 1,theta:size(1) do
-               for n = 1,theta:size(2) do
-                  theta[m][n] = theta_best[i][m][n]
-               end
-            end
-         end
-
-         -- do a forwrad pass across the nn
-         local dgt_out = nn.forward()
+         -- do a forwrad pass across the trained nn
+         local dgt_out = net:forward(data)
          return pred_digit(dgt_out)
 end
 
